dc7492e3062f67a0b4cffaac0ea0b3ef396f98a6
Luisalbertochinpro12 <al064359@uacam.mx>
2024-11-05 12:56:04 -0600

    feat: :see_no_evil: Creación de la interfaz

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,28 @@
-﻿namespace Hangman
+﻿using System.ComponentModel;
+
+namespace Hangman
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
+        List<String>  words = new List<String> ()
+            {"gato", "tito", "pato", "mayo", "coca",
+             "lola", "mamá", "peru", "lima", "agua",
+             "lalo", "tito", "tito", "tito", "tito"};
+
+        List<char> UserLetters = new List<char> ();
+
+        public void RandomWord()
+        {
+           Random random = new Random();
+           int num = random.Next() % words.Count;
+        }
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
+
+
     }
 
 }
