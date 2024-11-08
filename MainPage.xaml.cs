3675abef45d48022c347d7540904960af8a8024c
Luisalbertochinpro12 <al064359@uacam.mx>
2024-11-07 22:06:16 -0600

    feat: :see_no_evil: Optimización de las letras

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,51 +4,81 @@ namespace Hangman
 {
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
-        List<String>  words = new List<String> ()
+        public List<String> words = new List<String>()
             {"gato", "tito", "pato", "mayo", "coca",
-             "lola", "mamá", "peru", "lima", "agua",
+             "lola", "mami", "peru", "lima", "agua",
              "lalo", "aaac", "abce", "aaaa", "aaaab"};
 
-        List<char> UserLetters = new List<char> ();
+        public List<char> userletters = new List<char>();
+        public List<char> answer = new List<char>();
 
-        private List<char> letras = new List<char> ();
+        private List<char> letters = new List<char>();
 
-        public List<char> Letras
+        public List<char> Letters
         {
-            get => letras;
+            get => letters;
             set
             {
-                letras = value;
+                letters = value;
                 OnPropertyChanged();
             }
         }
 
         String word;
         private String spotlight = "";
-
-        private String SpotLight
+        public String Spotlight
         {
             get => spotlight;
-            set 
+            set
             {
                 spotlight = value;
                 OnPropertyChanged();
-             }
+            }
         }
 
         public String RandomWord()
         {
-           Random random = new Random();
-            int num = random.Next(0,14);
+            Random random = new Random();
+            int num = random.Next(0, 14);
 
-            return word = words [num];
+            answer.Add(Convert.ToChar(num));
+
+            return word = words[num];
+        }
+
+        public void BtnClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            button.IsEnabled = false;
+            char buttonletter = Convert.ToChar(button.Text);
+            CheckLetter(buttonletter);
+            button.Text = "";
+        }
+
+        public void CheckLetter (char letter)
+        {
+            if(userletters.IndexOf(letter) == -1)
+            {
+                userletters.Add(letter);
+            }
+            if(answer.IndexOf(letter) >= 0)
+            {
+                MaskWord(word, userletters);
+            }
+        }
+
+        private void MaskWord (String word, List<char> letters)
+        {
+            var mask = word.Select(index => letters.IndexOf(index) == 0 ? index : '_').ToArray();
+            Spotlight = string.Join(" ", mask);
         }
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = this;
             RandomWord();
-            Letras.AddRange("qwertyuiopasdfghjklzxcvbnm".ToCharArray());
+            Letters.AddRange("qwertyuiopasdfghjklzxcvbnm".ToCharArray());
+            BindingContext = this;
+            MaskWord(word, Letters);
         }
 
 
